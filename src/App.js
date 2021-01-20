a05674f7f4de5974fd576119166f729705770fd8
CCooper92 <am10ac17@gmail.com>
2021-01-20 15:50:38 -0800

    not done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import axios from 'axios';
+import { API_KAY, BASE_URL } from './components/index.js'
 
 function App() {
   return (
@@ -7,9 +9,13 @@ function App() {
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        <h2></h2>
+        <button onClick={() =>{}}>Click for Pictuer of the Day</button>
+        <img></img>
       </p>
     </div>
   );
 }
 
 export default App;
+
