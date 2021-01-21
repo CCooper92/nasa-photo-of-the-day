b02cd2d4315843d0b52482746ed664ef27b08096
CCooper92 <am10ac17@gmail.com>
2021-01-21 15:37:25 -0800

    styling almost done

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BASE_URL, API_KEY } from '../constants/keys';
+import styled from 'styled-components';
 
 const Display = () => {
 
@@ -17,24 +18,68 @@ const Display = () => {
     }, []);
 
     return (
-        <section class='main_content'>
+        <StyleSection>
+            
+            <StyleHead>
+              
+               <h1>NASA photo of the day</h1>
+               
+                
+            </StyleHead>
 
-            <section class='top-information'>
-                <h1>{pageData.title}</h1>
-                <p>{pageData.date}</p>
-            </section>
+            <StyleImg>
 
-            <section class='img'>
-                <a href={pageData.hdurl}><img src={pageData.url} alt='space' /></a>
-            </section>
+            <p>{pageData.date}</p>
+            <a href={pageData.hdurl}><img src={pageData.url} alt='space' /></a>
 
-            <section class='bottom_information'>
+            </StyleImg>
+
+            <StylePara>
+                <h2>{pageData.title}</h2>
                 <p>{pageData.explanation}</p>
-            </section>
+            </StylePara>
 
 
-        </section>
+        </StyleSection>
     )
 }
+
+const StyleSection = styled.section`
+    color: ${(pr) => pr.theme.secondaryColor};
+    background-color: ${(pr) => pr.theme.primaryColor};
+    display: flex;
+    flex-direction: column;
+     
+    @media (max-width: 550px) {
+        width: 100%;
+    }
+    `;
+
+    const StyleHead = styled.section`
+    color: ${(pr) => pr.theme.tertiaryColor};
+    `;
+
+
+    const StyleImg = styled.section`
+         img {
+            width: 50%;
+            border-radius: 20px
+         };
+        `;
+
+
+    const StylePara = styled.section`
+        p{
+            color: ${(pr) => pr.theme.tertiaryColor};
+            line-height:1.8;
+            padding: 1% 4% 1% 4%;
+        };
+    
+    `;
+    
+  
+    
+
+
 
 export default Display;
