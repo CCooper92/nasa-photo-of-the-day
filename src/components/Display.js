6804b9a78a1526f2be6ea0e302ddadb597e0e846
CCooper92 <am10ac17@gmail.com>
2021-01-21 19:22:03 -0800

    final styling

--- src/components/Display.js
+++ src/components/Display.js
@@ -27,17 +27,20 @@ const Display = () => {
                 
             </StyleHead>
 
-            <StyleImg>
+                <StyleContainer>
 
-            <p>{pageData.date}</p>
-            <a href={pageData.hdurl}><img src={pageData.url} alt='space' /></a>
+                    <StyleImg>
 
-            </StyleImg>
+                    <p>{pageData.date}</p>
+                    <a href={pageData.hdurl}><img src={pageData.url} alt='space' /></a>
 
-            <StylePara>
-                <h2>{pageData.title}</h2>
-                <p>{pageData.explanation}</p>
-            </StylePara>
+                    </StyleImg>
+
+                    <StylePara>
+                        <h2>{pageData.title}</h2>
+                        <p>{pageData.explanation}</p>
+                    </StylePara>
+             </StyleContainer>
 
 
         </StyleSection>
@@ -48,31 +51,109 @@ const StyleSection = styled.section`
     color: ${(pr) => pr.theme.secondaryColor};
     background-color: ${(pr) => pr.theme.primaryColor};
     display: flex;
-    flex-direction: column;
+    flex-direction:column;
+    
      
     @media (max-width: 550px) {
-        width: 100%;
+        width: 95%;
     }
     `;
 
     const StyleHead = styled.section`
     color: ${(pr) => pr.theme.tertiaryColor};
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    margin: 0 auto;
+    padding-bottom:-5%;
+    width:85%;
+    font-size:3rem;
+    border-bottom: 2px solid #D9CDB8;
+    
+
+    @media (max-width: 550px){
+            width: 100%;
+            
+            flex-direction:column;
+            justify-content:center;
+            align-content:center;
+            margin-left:5%;
+            font-size:1rem;
+            
+
+           
+        }
+
+
+   
+    
+    `;
+
+    const StyleContainer = styled.section`
+        display:flex;
+        max-width: 100%;
+
+        @media (max-width: 550px){
+            width: 100%;
+            
+            flex-direction:column;
+            justify-content:center;
+            align-content:center;
+            
+
+           
+        }
     `;
 
 
     const StyleImg = styled.section`
-         img {
-            width: 50%;
-            border-radius: 20px
+        width:70%;
+        display:flex;
+        flex-direction:column;
+        img {
+            width: 80%;
+            border-radius: 20px;
+            height: 95vh;
+            margin-bottom:5%;
          };
+         @media (max-width: 550px){
+            width: 100%;
+            justify-content:center;
+            align-content:center;
+            margin-left:4%;
+        }
+        
         `;
 
 
     const StylePara = styled.section`
         p{
             color: ${(pr) => pr.theme.tertiaryColor};
-            line-height:1.8;
-            padding: 1% 4% 1% 4%;
+            line-height:2.25;
+            
+            font-size: 1.3rem;
+            width: 90%;
+            
+        };
+        flex-direction: column;
+         max-width: 30%; 
+         display:flex;
+
+         @media (max-width: 550px){
+            max-width: 100%;
+            text-align:center;
+            margin-left:15%;
+           
+            justify-content:center;
+            align-content:center;
+
+            h2{
+                margin-right:5%;
+                margin-top:10%;
+            }
+
+            
+            
         };
     
     `;
